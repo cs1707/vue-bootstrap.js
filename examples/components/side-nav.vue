4e287cb44d6524b660a08a7aa5869f3d63282319
cs1707 <lost1q84@gmail.com>
2016-11-03 23:10:55 +0800

    change side-nav

--- examples/components/side-nav.vue
+++ examples/components/side-nav.vue
@@ -3,41 +3,31 @@
     <!-- <affix :offset-top="90"> -->
       <ul class="nav bs-docs-sidenav">
         <li>
-          <a href="javascript:void(0);">安装指南</a>
-          <ul class="nav sub-nav">
-            <li>
-              <router-link to="/components/quickstart">快速上手</router-link>
-            </li>
-            <li>
-              <router-link to="/components/development">开发指南</router-link>
-            </li>
-          </ul>
+          <router-link to="/components/quickstart">快速上手</router-link>
         </li>
         <li>
-          <a href="javascript:void(0);">组件</a>
-          <ul class="nav sub-nav">
-            <li>
-              <router-link to="/components/alert">Alert</router-link>
-            </li>
-            <li>
-              <router-link to="/components/affix">Affix</router-link>
-            </li>
-            <li>
-              <router-link to="/components/dropdown">Dropdown</router-link>
-            </li>
-            <li>
-              <router-link to="/components/modal">Modal</router-link>
-            </li>
-            <li>
-              <router-link to="/components/tab">Tab</router-link>
-            </li>
-            <li>
-              <router-link to="/components/progress">Progress</router-link>
-            </li>
-            <li>
-              <router-link to="/components/carousel">Carousel</router-link>
-            </li>
-          </ul>
+          <router-link to="/components/development">开发指南</router-link>
+        </li>
+        <li>
+          <router-link to="/components/alert">Alert</router-link>
+        </li>
+        <li>
+          <router-link to="/components/affix">Affix</router-link>
+        </li>
+        <li>
+          <router-link to="/components/dropdown">Dropdown</router-link>
+        </li>
+        <li>
+          <router-link to="/components/modal">Modal</router-link>
+        </li>
+        <li>
+          <router-link to="/components/tab">Tab</router-link>
+        </li>
+        <li>
+          <router-link to="/components/progress">Progress</router-link>
+        </li>
+        <li>
+          <router-link to="/components/carousel">Carousel</router-link>
         </li>
       </ul>
     <!-- </affix> -->
