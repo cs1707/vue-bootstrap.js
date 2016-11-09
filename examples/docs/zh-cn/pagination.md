46d5752f9d9037b6a74f89e4691168dcd5b38112
cs1707 <lost1q84@gmail.com>
2016-11-09 20:08:11 +0800

    pagination

--- examples/docs/zh-cn/pagination.md
+++ examples/docs/zh-cn/pagination.md
@@ -2,36 +2,40 @@
 
 ::: demo
 ``` html
-<bs-pagination>
-</bs-pagination>
+<bs-pagination :total-page="20" v-model="currentPage"></bs-pagination>
 ```
 :::
 
 ### 分页大小
 ::: demo
 ``` html
-<nav><bs-pagination size="lg"></bs-pagination></nav>
-<nav><bs-pagination></bs-pagination></nav>
-<nav><bs-pagination size="sm"></bs-pagination></nav>
+<nav><bs-pagination size="lg" v-model="currentPage" :total-page="5"></bs-pagination></nav>
+<nav><bs-pagination v-model="currentPage" :total-page="5"></bs-pagination></nav>
+<nav><bs-pagination size="sm" v-model="currentPage" :total-page="5"></bs-pagination></nav>
 ```
 :::
 
 ### 第一页最后一页按钮
 ::: demo
 ``` html
-<nav><bs-pagination :boundary-links="true"></bs-pagination></nav>
-<nav><bs-pagination :boundary-links="true" first-text="First" last-text="Last"></bs-pagination></nav>
+<nav><bs-pagination :boundary-links="true" v-model="currentPage" :total-page="5"></bs-pagination></nav>
+<nav><bs-pagination :boundary-links="true" first-text="First" last-text="Last" v-model="currentPage" :total-page="5"></bs-pagination></nav>
 ```
 :::
 
 ### 上一页下一页按钮
 ::: demo
 ``` html
-<nav><bs-pagination :direction-links="false"></bs-pagination></nav>
-<nav><bs-pagination previous-text="Previous" next-text="Next"></bs-pagination></nav>
+<nav><bs-pagination :direction-links="false" v-model="currentPage" :total-page="5"></bs-pagination></nav>
+<nav><bs-pagination previous-text="Previous" next-text="Next" v-model="currentPage" :total-page="5"></bs-pagination></nav>
 ```
 :::
 
+::: demo
+``` html
+<nav></nav>
+```
+:::
 
 | 参数      | 说明          | 类型      | 可选值                           | 默认值  |
 |---------- |-------------- |---------- |--------------------------------  |-------- |
@@ -42,4 +46,16 @@
 | last-text | 最后一页按钮文字 | String | - | » |
 | previous-text | 上一页按钮文字 | String | - | ‹ |
 | next-text | 下一页按钮文字 | String | - | › |
+| v-model | 当前页 | Number | - | - |
+| total-page | 总页数 | Number | - | - |
+
+<script>
+  export default {
+    data () {
+      return {
+        currentPage: 1
+      }
+    }
+  }
+</script>
 
--- src/components/pagination/index.vue
+++ src/components/pagination/index.vue
@@ -1,26 +1,43 @@
 <template>
   <ul class="pagination" :class="sizeClass">
-    <li v-if="boundaryLinks">
+    <li :class="{disabled: value <= 1}" v-if="boundaryLinks" @click="value > 1 && go(1)">
       <a href="javascript:void(0)">
         <span>{{firstText}}</span>
       </a>
     </li>
-    <li v-if="directionLinks">
+    <li :class="{disabled: value <= 1}" v-if="directionLinks" @click="value > 1 && go(value - 1)">
       <a href="javascript:void(0)">
         <span>{{previousText}}</span>
       </a>
     </li>
-    <li><a href="#">1</a></li>
-    <li><a href="#">2</a></li>
-    <li><a href="#">3</a></li>
-    <li><a href="#">4</a></li>
-    <li><a href="#">5</a></li>
-    <li v-if="directionLinks">
+
+    <li :class="{active: value == 1}" v-if="totalPage > 0" @click="go(1)">
+      <a href="javascript:void(0);">1</a>
+    </li>
+    <!-- previous more -->
+    <li v-if="showPreviousMore" @click="go(value - 5)">
+      <a href="javascript:void(0);">...</a>
+    </li>
+
+    <li :class="{active: value == i}" v-for="i in pages" @click="go(i)">
+      <a href="javascript:void(0);">{{i}}</a>
+    </li>
+
+    <!-- next more -->
+    <li v-if="showNextMore" @click="go(value + 5)">
+      <a href="javascript:void(0);">...</a>
+    </li>
+
+    <li :class="{active: value == totalPage}" v-if="totalPage > 1" @click="go(totalPage)">
+      <a href="javascript:void(0);">{{totalPage}}</a>
+    </li>
+
+    <li v-if="directionLinks" :class="{disabled: value >= totalPage}" @click="value < totalPage && go(value + 1)">
       <a href="javascript:void(0)">
         <span>{{nextText}}</span>
       </a>
     </li>
-    <li v-if="boundaryLinks">
+    <li v-if="boundaryLinks" :class="{disabled: value >= totalPage}" @click="value < totalPage && go(totalPage)">
       <a href="javascript:void(0)">
         <span>{{lastText}}</span>
       </a>
@@ -58,14 +75,84 @@
       nextText: {
         type: String,
         default: '›'
+      },
+      value: Number,
+      totalPage: Number
+    },
+    data () {
+      return {
+        showPreviousMore: false,
+        showNextMore: false
       }
     },
     computed: {
-      sizeClass (val) {
-        if (!val) {
+      sizeClass () {
+        if (!this.size) {
           return []
         }
         return [ `pagination-${this.size}` ]
+      },
+      pages () {
+        const count = 7
+
+        const currentPage = Number(this.value)
+        const totalPage = Number(this.totalPage)
+
+        let showPreviousMore = false
+        let showNextMore = false
+
+        if (totalPage > count) {
+          if (currentPage > count - 2) {
+            showPreviousMore = true
+          }
+
+          if (currentPage < totalPage - 2) {
+            showNextMore = true
+          }
+        }
+
+        this.showPreviousMore = showPreviousMore
+        this.showNextMore = showNextMore
+
+        const list = []
+
+        if (showPreviousMore && !showNextMore) {
+          const start = totalPage - (count - 2)
+          for (let i = start; i < totalPage; i++) {
+            list.push(i)
+          }
+        } else if (!showPreviousMore && showNextMore) {
+          for (let i = 2; i < count; i++) {
+            list.push(i)
+          }
+        } else if (showPreviousMore && showNextMore) {
+          const offset = Math.floor(count / 2) - 1
+          for (let i = currentPage - offset; i <= currentPage + offset; i++) {
+            list.push(i)
+          }
+        } else {
+          for (let i = 2; i < totalPage; i++) {
+            list.push(i)
+          }
+        }
+
+        return list
+      }
+    },
+    methods: {
+      go (page) {
+        if (!Number.isNaN(page)) {
+          if (page < 1) {
+            page = 1
+          }
+          if (page > this.totalPage) {
+            page = this.totalPage
+          }
+        }
+        if (this.value !== page) {
+          this.$emit('on-change', page)
+        }
+        this.$emit('input', page)
       }
     }
   }
